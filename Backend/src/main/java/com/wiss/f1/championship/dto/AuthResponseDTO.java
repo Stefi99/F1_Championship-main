eb14c6ffa0c5b46667469b04cb793bc506db8529
Stefi99 <stephanie.martins.lima@hotmail.com>
2025-12-10 10:11:36 +0100

    stefi: getter und setter hinzufügen bei AuthRequestDTO und AuthResponseDTO

--- Backend/src/main/java/com/wiss/f1/championship/dto/AuthResponseDTO.java
+++ Backend/src/main/java/com/wiss/f1/championship/dto/AuthResponseDTO.java
@@ -7,6 +7,9 @@ public class AuthResponseDTO {
     private String role;
     private String token;
 
+    public AuthResponseDTO() {
+    }
+
     public AuthResponseDTO(Long id, String username, String role, String token) {
         this.id = id;
         this.username = username;
@@ -15,7 +18,14 @@ public class AuthResponseDTO {
     }
 
     public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
     public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
     public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+
     public String getToken() { return token; }
+    public void setToken(String token) { this.token = token; }
 }
