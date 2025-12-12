3d86d4efc62ed54779636bffc315424e267d09c1
Stefi99 <stephanie.martins.lima@hotmail.com>
2025-12-12 22:30:33 +0100

    stefi: rest kommentare

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,3 +1,20 @@
+/**
+ * main.jsx - Einstiegspunkt der React-Anwendung
+ *
+ * Diese Datei ist der Einstiegspunkt für die gesamte F1 Championship Anwendung.
+ * Sie rendert die Root-Komponente in das DOM und stellt die notwendigen
+ * Provider-Komponenten bereit:
+ *
+ * - React.StrictMode: Aktiviert zusätzliche Entwickler-Warnungen
+ * - AuthProvider: Stellt Authentifizierungs-Kontext für die gesamte App bereit
+ * - BrowserRouter: Ermöglicht Client-seitiges Routing
+ * - AppRouter: Definiert alle Routen der Anwendung
+ *
+ * Die Reihenfolge der Provider ist wichtig:
+ * 1. AuthProvider muss außen sein, damit alle Komponenten Zugriff auf Auth-Kontext haben
+ * 2. BrowserRouter muss innerhalb von AuthProvider sein, damit Router-Hooks funktionieren
+ * 3. AppRouter verwendet beide Kontexte für Routing und Authentifizierung
+ */
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -7,12 +24,22 @@ import AppRouter from "./router/AppRouter.jsx";
 
 import "./index.css";
 
-// Einstiegspunkt der App
-// AuthProvider und BrowserRouter umschließen die gesamte Anwendung
+/**
+ * Rendert die Anwendung in das DOM
+ *
+ * Die Komponenten-Hierarchie:
+ * React.StrictMode
+ *   └─ AuthProvider (Authentifizierungs-Kontext)
+ *       └─ BrowserRouter (Router-Kontext)
+ *           └─ AppRouter (Route-Definitionen)
+ */
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    {/* AuthProvider: Stellt Authentifizierungs-State für die gesamte App bereit */}
     <AuthProvider>
+      {/* BrowserRouter: Ermöglicht Client-seitiges Routing mit react-router-dom */}
       <BrowserRouter>
+        {/* AppRouter: Definiert alle Routen (Home, Auth, Player, Admin) */}
         <AppRouter />
       </BrowserRouter>
     </AuthProvider>
